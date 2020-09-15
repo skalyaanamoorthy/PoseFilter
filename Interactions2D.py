e6e773d42ba048e0089815c3ceaa440426d573f8
justine <justinecwilliams@gmail.com>
2020-09-15 00:19:32 -0400

    Fixed bug.

--- Interactions2D.py
+++ Interactions2D.py
@@ -2,7 +2,7 @@ import pymol
 import os
 from pymol import cmd
 import oddt
-import oddt.interactions as interactions
+from .oddt_interactions import *
 #import os
 import numpy as np
 import glob
@@ -45,35 +45,35 @@ def InteractionCheck(proteinpath, Listoflig):
         ligand = next(oddt.toolkit.readfile('pdb', ligandname))
 
         # Hydrophobic interactions
-        p_hydroph, l_hydroph = interactions.hydrophobic_contacts(protein, ligand)
+        p_hydroph, l_hydroph = hydrophobic_contacts(protein, ligand)
         InteractionsFile(p_hydroph, l_hydroph, path, 'hydrophobic')
 
         # h bonds
-        p_hbonds, l_hbonds, strict = interactions.hbonds(protein, ligand)
+        p_hbonds, l_hbonds, strict = hbonds(protein, ligand)
         InteractionsFile(p_hbonds, l_hbonds, path, 'hydrogen bond')
 
         # halogens
-        p_halogen, l_halogen, strict = interactions.halogenbonds(protein, ligand)
+        p_halogen, l_halogen, strict = halogenbonds(protein, ligand)
         InteractionsFile(p_halogen, l_halogen, path, 'halogen bond')
 
         # pistacking bonds
-        pi_interactions = interactions.pi_stacking(protein, ligand)
+        pi_interactions = pi_stacking(protein, ligand)
         InteractionsFile(pi_interactions[0], pi_interactions[2], path, 'pi stacking')
 
         # salt bridges
-        p_salt_bridges, l_salt_bridges = interactions.salt_bridges(protein, ligand)
+        p_salt_bridges, l_salt_bridges = salt_bridges(protein, ligand)
         InteractionsFile(p_salt_bridges, l_salt_bridges, path, 'salt bridge')
 
         # pi_cation
-        p_pi_cation, l_pi_cation, strict = interactions.pi_cation(protein, ligand)
+        p_pi_cation, l_pi_cation, strict = pi_cation(protein, ligand)
         InteractionsFile(p_pi_cation, l_pi_cation, path, 'pi cation')
 
         # acceptor_metal bonds
-        p_acceptor_metal, acceptor_metal, strict = interactions.acceptor_metal(protein, ligand)
+        p_acceptor_metal, acceptor_metal, strict = acceptor_metal(protein, ligand)
         InteractionsFile(p_acceptor_metal, acceptor_metal, path, 'acceptor metal')
 
         # pi_metal bonds
-        p_pi_metal, l_pi_metal, strict = interactions.pi_metal(protein, ligand)
+        p_pi_metal, l_pi_metal, strict = pi_metal(protein, ligand)
         InteractionsFile(p_pi_metal, l_pi_metal, path, 'pi metal')
 
 
